7a3ba2f2f8dc4c094672730263ec125c565df629
Cesar <tratohecho3@gmail.com>
2018-06-22 11:47:51 -0400

    sass and bulma implemented

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss'
+
+</style>
